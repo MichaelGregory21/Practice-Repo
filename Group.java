9626f78eeb47aa7796afc162b018a63b68507d18
Woojuro Shin <wshin@mun.ca>
2024-03-28 14:49:27 -0230

    ramdomGames method implemented

--- Group.java
+++ Group.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * Write a description of class Group here.
@@ -7,8 +8,9 @@
  */
 public class Group
 {
-    final int NUM_OF_TEAMS = 5;
-    Team[] teams;
+    private final int NUM_OF_TEAMS = 5;
+    private Team[] teams;
+    private ArrayList<Game> games;
 
     /**
      * Constructor for objects of class Group
@@ -18,6 +20,16 @@ public class Group
         teams = new Team[NUM_OF_TEAMS];
     }
 
+    public void randomGames() {
+        for (int i = 0; i < (NUM_OF_TEAMS -1); i++)
+        {
+            for (int j = 1; j < NUM_OF_TEAMS; j++){
+                Game game = new Game(teams[i], teams[j]);
+                games.add(game);
+            }
+        }
+    }
+    
     public void canculateStanding()
     {
         
