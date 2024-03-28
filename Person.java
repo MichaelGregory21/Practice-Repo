ed09de6b2ca9fd3fd697f83ff5a3ee38cab142fe
Michael Gregory <mtgregory@mun.ca>
2024-03-28 14:39:53 -0230

    Implemented Player, Coach, abstract Person

--- Person.java
+++ Person.java
@@ -5,29 +5,17 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Person
+public abstract class Person
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    private String name;
+    
     /**
      * Constructor for objects of class Person
      */
     public Person()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    public abstract void playRole();
 }
